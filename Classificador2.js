e27ef78e62d1ab396064aed31d4bd10c362c59eb
Laeet <laet.neto@gmail.com>
2023-11-16 19:57:01 -0300

    first commit

---
+++ Classificador2.js
@@ -0,0 +1,40 @@
+
+console.log("Bem Vindo.")
+let vitorias = 100
+
+function Classificador(vitorias){
+    let classificacao = []
+if (vitorias < 10) 
+{
+ return classificacao = "Ferro";
+}
+else if (vitorias >10 && vitorias<=20)
+{
+    return classificacao= "Bronze";
+}
+else if (vitorias >20 && vitorias<=50)
+{
+    return classificacao= "Prata";
+}
+else if (vitorias >50 && vitorias<=80)
+{
+    return classificacao= "Ouro";
+}
+else if (vitorias >80 || vitorias<=90)
+{
+    return classificacao= "Diamante";
+}
+else if (vitorias >90 || vitorias<=100)
+{
+    return classificacao= "Lendário";
+}
+else if (vitorias >=101)
+{
+    return classificacao= "Imortal";
+}
+
+function resultado(vitorias) {
+}
+
+
+}
